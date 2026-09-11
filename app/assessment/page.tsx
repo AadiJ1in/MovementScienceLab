b6ac6006f2cd8931b342577426cd6dd8e6bfbe6d
AadiJ1in <ajtexas2022@gmail.com>
2026-09-11 14:21:45 -0400

    Add guided assessment route

---
+++ app/assessment/page.tsx
@@ -0,0 +1,9 @@
+import { GuidedAssessment } from "@/components/assessment/GuidedAssessment";
+
+export default function AssessmentPage() {
+  return (
+    <main className="min-h-screen bg-[radial-gradient(circle_at_top,#e0f2fe_0,transparent_34%),linear-gradient(#fafafa,#f4f4f5)]">
+      <GuidedAssessment />
+    </main>
+  );
+}
