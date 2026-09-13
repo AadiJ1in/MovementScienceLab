4cee5ca37e1f3bec5d00d1b521a9933e313a8338
AadiJ1in <ajtexas2022@gmail.com>
2026-09-13 14:18:08 -0400

    Expose single-leg knee-control research assessment

---
+++ app/assessment/single-leg-knee-control/page.tsx
@@ -0,0 +1,9 @@
+import { SingleLegKneeControlAssessment } from "@/components/assessment/SingleLegKneeControlAssessment";
+
+export default function SingleLegKneeControlPage() {
+  return (
+    <main className="min-h-screen bg-[#f5f5f2]">
+      <SingleLegKneeControlAssessment />
+    </main>
+  );
+}
